14d696e15401599b30bfccbffc6ca8e4f831df4b
juhamikael <contact.juhamusic@gmail.com>
2023-11-14 20:35:54 +0200

    I'm not sure why this route is being created here but whatever

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db";
+import { users } from "@/lib/db/schema/users";
+import { NextResponse } from "next/server";
+
+type Users = {
+    id: string;
+    firstName: string | null;
+    lastName: string | null;
+    email: string | null;
+};
+
+export async function GET() {
+    const result: Users[] = await db.select().from(users);
+    return NextResponse.json({
+        status: 200,
+        body: result,
+    });
+}
