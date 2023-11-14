7f0079468bbccb439980297c9270aab242a34ec6
juhamikael <contact.juhamusic@gmail.com>
2023-11-14 23:42:18 +0200

    Migrations

--- src/lib/db/migrations/0000_nosy_bruce_banner.sql
+++ src/lib/db/migrations/0000_nosy_bruce_banner.sql
@@ -24,12 +24,12 @@ CREATE TABLE `orders` (
 );
 --> statement-breakpoint
 CREATE TABLE `categories` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `products` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY NOT NULL,
 	`imageId` text NOT NULL,
 	`name` text NOT NULL,
 	`price` integer NOT NULL,
@@ -42,9 +42,9 @@ CREATE TABLE `products` (
 );
 --> statement-breakpoint
 CREATE TABLE `subcategories` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
-	`categoryId` integer,
+	`categoryId` text,
 	FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
